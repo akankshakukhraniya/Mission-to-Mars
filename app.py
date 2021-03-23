413c193d08eb00ef094c6873710344368fba222c
akankshakukhraniya <73720327+akankshakukhraniya@users.noreply.github.com>
2021-03-22 23:55:06 -0400

    Web scraping assignment

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from scrape_mars import scrape
+
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+@app.route('/')
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars
+    data = scrape()
+    mars.update(
+        {},
+        data,
+        upsert=True
+    )
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,76 @@
+
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+executable_path = {'executable_path': ChromeDriverManager().install()}
+
+def scrape():
+    try:  
+        browser = Browser('chrome', **executable_path, headless=False)
+        title, paragraph = news(browser)
+        mars = {
+            'title': title,
+            'paragraph': paragraph,
+            'image': get_image(browser),
+            'facts': facts(),
+            'hemispheres': hemi(browser)
+        }
+        return mars
+    except TimeoutError:
+                mars = {
+                            'title': "title",
+                            'paragraph': "paragraph",
+                            'image': "get_image(browser)",
+                            'facts': "facts()",
+                            'hemispheres': "hemi(browser)"
+                        }
+                return mars
+
+# Visit Mars news URL page 
+def news(browser):
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    mars_news_title = soup.find('li', class_='slide')
+    news_title = mars_news_title.find('div', class_='content_title').text
+    news_para = mars_news_title.find('div', class_="article_teaser_body").text
+    return news_title, news_para
+
+# ## Scraping JPL Featured Image URL
+# visit the JPL Featured Space Image website
+def get_image(browser):
+    jpl_img_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(jpl_img_url)
+    browser.click_link_by_partial_text('FULL IMAGE')
+    return browser.find_by_css('img.fancybox-image')['src']
+
+# Visit the Mars Facts webpage
+def facts():
+    facts_webpg = 'https://space-facts.com/mars/'
+    browser.visit(facts_webpg)
+    html = browser.html
+    table = pd.read_html(html)
+    facts_df = table[0]
+    facts_df.columns = ['Description', 'Value']
+    facts_df['Description'] = facts_df['Description'].str.replace(':','')
+    return facts_df.to_html()
+
+# Visit the USGS Astrogeology site 
+def hemi(browser):
+    USGS_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(USGS_url)
+    links = browser.find_by_css('a.itemLink h3')
+    browser.back()
+    hemispheres = []
+    for i in range(len(links)):
+        hemisphere = {}
+        hemisphere['title'] = browser.find_by_css('a.itemLink h3')[i].text
+        browser.find_by_css('a.itemLink h3')[0].click()
+        hemisphere['url'] = browser.find_link_by_partial_text('Sample')['href']
+        hemispheres.append(hemisphere)    
+        browser.back()
+    browser.quit()
+    return hemispheres
